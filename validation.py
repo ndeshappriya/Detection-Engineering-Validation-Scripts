3bb1d978d2270e52f89e561d544f9d1687872fc5
Nimantha Deshappriya <nimantha.deshappriya@coolbet.com>
2023-12-17 22:01:36 +0200

    first commit

---
+++ validation.py
@@ -0,0 +1,40 @@
+import tomllib
+import sys
+import os
+
+  # with open(full_path,"rb") as toml:
+  # alert = tomllib.load(toml)
+             
+
+for root, dirs, files in os.walk("C:\\Users\\nimantha.deshappriya\\Documents\\Detection Engineering\\customalerts"):
+    for file in files:
+        if file.endswith(".toml"):
+           full_path = os.path.join(root, file)
+           with open(full_path,"rb") as toml:
+                     alert = tomllib.load(toml)
+             
+             
+                     present_fields = []
+                     missing_fields = []
+
+                     if alert['rule']['type'] == "query": #query based alert
+                           required_fields = ['description','name','risk_score','severity','type','query']
+                     elif alert['rule']['type'] == "eql": #event correlation alert
+                           required_fields = ['description','name','risk_score','severity','type','query','language']
+                     elif alert['rule']['type'] == "threshold": #treshold based alert
+                           required_fields = ['description','name','risk_score','severity','type','query','threshold']
+                     else:
+                         print("Unsupported rule type found in: " + full_path) 
+                         break
+                     for table in alert:
+                        for field in alert [table]:
+                           present_fields.append(field)
+
+                     for field in required_fields:
+                        if field not in present_fields:
+                           missing_fields.append(field)
+
+                     if missing_fields:
+                        print("The following fields do not exist in " + file + ":" + str(missing_fields))
+                     else: 
+                        print("validation pass for: " + file)
